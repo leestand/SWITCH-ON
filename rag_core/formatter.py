1d6c4b50940e3f400b122159bdcd21cceaeba724
sujeongiyo <ssi0929@naver.com>
2025-06-11 15:22:36 +0900

    Delete rag_core directory

--- rag_core/formatter.py
+++
@@ -1,6 +0,0 @@
-
-def format_docs_optimized(docs):
-    return "\n\n".join(
-        f"[출처: {doc.metadata.get('source', '출처없음')}]\n{doc.page_content}"
-        for doc in docs
-    )
--- rag_core/memory.py
+++
@@ -1,8 +0,0 @@
-
-from langchain_community.chat_message_histories import ChatMessageHistory
-from langchain_core.runnables.history import RunnableWithMessageHistory
-
-def get_session_history():
-    def get_by_session_id(session_id: str):
-        return ChatMessageHistory()
-    return get_by_session_id
