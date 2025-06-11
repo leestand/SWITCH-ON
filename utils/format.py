4044cce6dee35042c1bfe56bae116198f2de6e0c
sujeongiyo <ssi0929@naver.com>
2025-06-11 15:30:44 +0900

    Delete utils directory

--- utils/format.py
+++
@@ -1,8 +0,0 @@
-def format_docs_optimized(docs, search_type):
-    if not docs:
-        return "관련 자료를 찾을 수 없습니다."
-    results = []
-    for i, doc in enumerate(docs):
-        content = doc.page_content[:500]
-        results.append(f"[{i+1}] {content}...")
-    return "\n\n".join(results)
